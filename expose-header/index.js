51888d5d19e56ee160f6b9498b9846929a08cd05
Sthembile-code <54074371+Sthembile-code@users.noreply.github.com>
2022-06-02 17:27:10 +0200

    Update index.js

--- expose-header/index.js
+++ expose-header/index.js
@@ -12,3 +12,50 @@ module.exports.init = function(config, logger, stats) {
 
   };
 }
+/*module.exports.init = function(config, logger, stats){
+		//Initialisation (for each worker thread started)
+	var plugId = "-- PT: ";
+	    var idMsg = plugId + ' Provide-TransactionId';
+		
+		var idPrefix;
+		if (config.active) {
+			idMsg += " initialised and active";
+			idPrefix = config.prefix ? config.prefix : "emg-:";
+			idMsg += ". prefix is " + idPrefix;		
+		} else {
+			idMsg += ' present but inactive.';
+		}
+		console.log(idMsg);
+		
+		//Check if crypto support is in NodeJS core - in case need to generate a GUID
+		//The fastest possible way to create random 32-char string in Node is by using native crypto module:
+		// ocnst id = crypto.randomBytes(16).toString("hex");
+		//let crypto;
+		//try {
+			//crypto = require('crypto');
+			//console.log(plugId + 'Supported crypto hashes :' + crypto.getHashes() );
+			//
+			//} catch (err) {
+		    //console.log(plugId + 'crypto support is disabled!');
+			//}
+			
+		// on traffic...
+		return {
+			
+			onrequest: function(req, res, next) {
+				
+				if (config.active) {
+					if (!req.headers['mtnTxID']) {
+						try {
+							 
+							 req.headers['mtnTxID'] = idPrefix + req.correlationId;
+						} catch (e) {}
+					}
+				}
+				
+				//== Traffic must flow to next (plugin)
+				next();
+			}
+			
+		}
+	}
